e5a5ec3c91e9c76477eaa66b31e7aead324d73ef
Mohan Barde <147102911+krishna7805@users.noreply.github.com>
2023-10-14 00:55:46 +0530

    Create Program to Check Alphabet.c

---
+++ Program to Check Alphabet.c
@@ -0,0 +1,23 @@
+// 25-Days-75-Question-C-Coding-Challenge
+// Day 6
+// program no. 16 : Program to Check Alphabet
+
+
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Enter Character \n");
+    scanf("%c", &c);
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+    printf("%c is an alphabet \n", c);
+    
+    else
+    printf("%c is not an alphabet \n", c);
+    
+}
+    
+    return 0 ;
+    
+}
