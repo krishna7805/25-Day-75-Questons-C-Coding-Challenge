2f9d8a73d69f3e8ec13705966231a0e26f8c755e
Mohan Barde <147102911+krishna7805@users.noreply.github.com>
2023-10-14 22:15:58 +0530

    Create 19. C Program to Generate Multiplication Table.c

---
+++ 19. C Program to Generate Multiplication Table.c
@@ -0,0 +1,16 @@
+// 25-Days-75-Question-C-Coding-Challenge
+// Day 07
+// Question no. 19: C Program to Generate Multiplication Table
+
+#include <stdio.h>
+int main() {
+  int a,m;
+  printf("Enter an integer: ");
+  scanf("%d", &a);
+
+  for (int i = 1; i <= 10; ++i) {
+      m = a * i;
+    printf("%d * %d = %d \n", a, i,m);
+  }
+  return 0;
+}
