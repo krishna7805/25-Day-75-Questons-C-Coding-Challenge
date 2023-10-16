9753361dfc8f7acd917ca37965f636c4a8f5079c
Mohan Barde <147102911+krishna7805@users.noreply.github.com>
2023-10-16 17:05:07 +0530

    Create 23. C Program to Display Characters from A to Z Using Loop.c

---
+++ 23. C Program to Display Characters from A to Z Using Loop.c
@@ -0,0 +1,11 @@
+// 25-Days-75-Question-C-Coding-Challenge
+// day 08
+// Question number 23 : C Program to Display Characters from A to Z Using Loop
+
+#include<stdio.h>
+int main() {
+    char c;
+    for (c = 'A'; c <= 'Z'; ++c)
+        printf("%c ", c);
+    return 0;
+}
