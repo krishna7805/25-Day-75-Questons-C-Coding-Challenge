48577d8cfb79360d0f674a294f8d0ffb60f8b4fd
Mohan Barde <147102911+krishna7805@users.noreply.github.com>
2023-10-18 14:55:50 +0530

    Create 28. C Program to Check Whether a Number is Prime or Not.c

---
+++ 28. C Program to Check Whether a Number is Prime or Not.c
@@ -0,0 +1,35 @@
+//25-Days-75-Question-C-Coding-Challange
+// Day 10
+// Question No. 28 : C Program to Check Whether a Number is Prime or Not
+
+#include<stdio.h>
+
+int main() {
+
+  int n, i, flag = 0;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+
+  // 0 and 1 are not prime numbers
+  // change flag to 1 for non-prime number
+  if (n == 0 || n == 1)
+    flag = 1;
+
+  for (i = 2; i <= n / 2; ++i) {
+
+    // if n is divisible by i, then n is not prime
+    // change flag to 1 for non-prime number
+    if (n % i == 0) {
+      flag = 1;
+      break;
+    }
+  }
+
+  // flag is 0 for prime numbers
+  if (flag == 0)
+    printf("%d is a prime number.", n);
+  else
+    printf("%d is not a prime number.", n);
+
+  return 0;
+}
