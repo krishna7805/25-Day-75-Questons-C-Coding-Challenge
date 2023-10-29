9e16864759a7c8684c37d47b7e9677210ef56e93
Mohan Barde <147102911+krishna7805@users.noreply.github.com>
2023-10-29 17:24:32 +0530

    Create 46. C Program to Find Largest Element in an Array.c

---
+++ 46. C Program to Find Largest Element in an Array.c
@@ -0,0 +1,27 @@
+// 25-Days-75-question-C-Challenge
+// Day 16 : 21-oct-2023
+// Question no. 46 : C Program to Calculate Average Using Arrays
+
+#include <stdio.h>
+int main() {
+  int n;
+  double arr[100];
+  printf("Enter the number of elements (1 to 100): ");
+  scanf("%d", &n);
+
+  for (int i = 0; i < n; ++i) {
+    printf("Enter number%d: ", i + 1);
+    scanf("%lf", &arr[i]);
+  }
+
+  // storing the largest number to arr[0]
+  for (int i = 1; i < n; ++i) {
+    if (arr[0] < arr[i]) {
+      arr[0] = arr[i];
+    }
+  }
+
+  printf("Largest element = %.2lf", arr[0]);
+
+  return 0;
+}
