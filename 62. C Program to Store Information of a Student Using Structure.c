7fd5700a0ec3fb75dc4b100f78568aa446758d55
Mohan Barde <147102911+krishna7805@users.noreply.github.com>
2023-10-29 18:18:00 +0530

    Create 62. C Program to Store Information of a Student Using Structure.c

---
+++ 62. C Program to Store Information of a Student Using Structure.c
@@ -0,0 +1,29 @@
+// 25-Days-75-question-C-Challenge
+// Day 21 : 25-oct-2023
+// Question no. 62 : C Program to Store Information of a Student Using Structure
+
+#include <stdio.h>
+struct student {
+    char name[50];
+    int roll;
+    float marks;
+} s;
+
+int main() {
+    printf("Enter information:\n");
+    printf("Enter name: ");
+    fgets(s.name, sizeof(s.name), stdin);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll);
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    printf("Displaying Information:\n");
+    printf("Name: ");
+    printf("%s", s.name);
+    printf("Roll number: %d\n", s.roll);
+    printf("Marks: %.1f\n", s.marks);
+
+    return 0;
+}
